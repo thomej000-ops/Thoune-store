c90e7681c2ad8bbe75c0cecec9c1a95656fd555e
thomej000-ops <thomej000@gmail.com>
2026-09-12 18:33:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ async function updateAccountUI() {
   if (session?.user) {
     accountDetails.classList.remove("hidden");
 accountEmail.textContent = session.user.email || "";
+    console.log("CONTA LOGADA:", session.user.email);
     accountMessage.textContent = "Você está conectado à sua conta.";
     loginButton.classList.add("hidden");
     signupButton.classList.add("hidden");
