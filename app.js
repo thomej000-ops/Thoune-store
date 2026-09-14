644a89008d1acb080f78ae1e656d720aa9465ced
thomej000-ops <thomej000@gmail.com>
2026-09-13 21:31:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ accountDetails.classList.remove("hidden");
 accountEmail.textContent = session.user.email || "";
 profileFields.classList.remove("hidden");
 await loadProfile(session.user.id);
+console.log("TESTE: carregando pedidos...");
 await loadOrders(session.user.id);
 profileFields.style.display = "block";
 accountMessage.textContent = "Você está conectado à sua conta.";
