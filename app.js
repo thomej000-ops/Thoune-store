7bb5e0d8fe04aa929c18ebe8f85146842225f4aa
thomej000-ops <thomej000@gmail.com>
2026-09-13 21:39:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -80,7 +80,6 @@ accountDetails.classList.remove("hidden");
 accountEmail.textContent = session.user.email || "";
 profileFields.classList.remove("hidden");
 await loadProfile(session.user.id);
-toast("TESTE: conta carregada!");
 await loadOrders(session.user.id);
 profileFields.style.display = "block";
 accountMessage.textContent = "Você está conectado à sua conta.";
